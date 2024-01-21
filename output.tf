d6b9bc11c8887d3198c5078ad3b4723fbd4b3cc2
saite <saitejasai57@gmail.com>
2024-01-21 11:49:34 +0530

    new

--- output.tf
+++ output.tf
@@ -1,11 +1,15 @@
-output "azs" {
-  value = data.aws_availability_zones.roboshop_vpc_az.names
-}
+# output "azs" {
+#   value = data.aws_availability_zones.roboshop_vpc_az.names
+# }
 
-output "default_vpc_id" {
-  value = data.aws_vpc.default.id
-}
+# output "default_vpc_id" {
+#   value = data.aws_vpc.default.id
+# }
 
-output "default_routetable_id" {
-  value = data.aws_route_table.default.id
+# output "default_routetable_id" {
+#   value = data.aws_route_table.default.id
+# }
+
+output "vpc_id" {
+  value = aws_vpc.roboshop-vpc.id
 }
